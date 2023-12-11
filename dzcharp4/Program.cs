6e544a870f9a83927c0790e2b7c9d465158ec5b6
Gera <sergeev9927@gmail.com>
2023-12-11 19:17:30 +0300

    3 zadazha

--- dzcharp4/Program.cs
+++ dzcharp4/Program.cs
@@ -69,14 +69,62 @@
 //  2 6 9
 //  4 6 2 
 
-int[,] array = new int[4, 4]
-{  { 1, 2, 3, 4 },
-   { 2, 2, 2, 2 },
-   { 1, 1, 1, 1 },
-   { 5, 6, 7, 8 }
-};
-int max = 0;
-for (int i = 0; i < array.GetLength(1); i++)
+Console.Write("Введите колличество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m, n];
+
+void CreateArray(int m, int n)
 {
-    max = max + array[i];
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            randomArray[i, j] = rand.Next(1, 9);
+        }
+    }
 }
+
+void PrintArray(int[,] array)
+{
+    int i, j;
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{ array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+CreateArray(m, n);
+Console.WriteLine("\nИсходный массив: ");
+PrintArray(randomArray);
+
+int SumLine(int[,] array, int i)
+{
+    int sum = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    return sum;
+}
+
+int minSum = 1;
+int sum = SumLine(randomArray, 0);
+for (int i = 1; i < randomArray.GetLength(0); i++)
+{
+    if (sum > SumLine(randomArray, i))
+    {
+        sum = SumLine(randomArray, i);
+        minSum = i + 1;
+    }
+}
+Console.WriteLine($"\nСтрока c наименьшей суммой элементов: { minSum}");
+
